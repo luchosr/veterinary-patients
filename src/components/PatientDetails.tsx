960342dae0b48c3d704475290d99ddc1d03c86d2
Luciano Ramello <luchosr@gmail.com>
2024-09-12 13:49:56 +0200

    feat: implements patients deletion on patient details

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from '../store/store';
 import { Patient } from '../types';
 import PatientDetailItem from './PatientDetailItem';
 
@@ -6,6 +7,8 @@ type PatientDetailsProps = {
 };
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+  const deletePatient = usePatientStore((state) => state.deletePatient);
+
   return (
     <div className="mx-5 my-10  px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="ID" data={patient.id} />
@@ -25,6 +28,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
         >
           Eliminar
         </button>
