4ad6dc3d03ee4e8f9e554bd3641afd6f9af0eae2
Luciano Ramello <luchosr@gmail.com>
2024-09-12 10:54:10 +0200

    feat: implements add patient action on store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
-import { Patient } from './../types/index';
+import { DraftPatient, Patient } from './../types/index';
 import { create } from 'zustand';
 
 type PatientState = {
   patients: Patient[];
+  addPatient: (data: DraftPatient) => void;
 };
 
 export const usePatientStore = create<PatientState>(() => ({
   patients: [],
+  addPatient: (data: DraftPatient) => {
+    console.log(data);
+  },
 }));
