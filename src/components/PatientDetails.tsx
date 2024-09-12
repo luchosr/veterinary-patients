961161dc3ea191810565348fc06d24c04e09d2f1
Luciano Ramello <luchosr@gmail.com>
2024-09-12 12:55:47 +0200

    feat: adds patient details component

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,9 @@
+import { Patient } from '../types';
+
+type PatientDetailsProps = {
+  patient: Patient;
+};
+
+export default function PatientDetails({ patient }: PatientDetailsProps) {
+  return <div>{patient.name}</div>;
+}
