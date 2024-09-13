f3ecb0105010e6e6a1473774353fda70f71c5cf4
Luciano Ramello <luchosr@gmail.com>
2024-09-13 09:46:19 +0200

    feat: implement toast on patient deletion

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 import { usePatientStore } from '../store/store';
 import { Patient } from '../types';
 import PatientDetailItem from './PatientDetailItem';
@@ -10,6 +11,10 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
   const deletePatient = usePatientStore((state) => state.deletePatient);
   const getPatientById = usePatientStore((state) => state.getPatientById);
 
+  const handleClick = () => {
+    deletePatient(patient.id);
+    toast.error('Paciente eliminado');
+  };
   return (
     <div className="mx-5 my-10  px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="ID" data={patient.id} />
@@ -30,7 +35,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-          onClick={() => deletePatient(patient.id)}
+          onClick={handleClick}
         >
           Eliminar
         </button>
